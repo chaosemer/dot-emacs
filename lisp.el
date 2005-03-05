c18d8291bdb37a555815f0649ee77a26030fbe7a
jared <jared@4aac56dc-aff8-45ad-a6cf-605ace86fc27>
2005-03-05 04:00:49 +0000

    *** empty log message ***
    git-svn-id: file:///home/jared/Desktop/vc/svn-dot-emacs/trunk@7 4aac56dc-aff8-45ad-a6cf-605ace86fc27

--- lisp.el
+++ lisp.el
@@ -3,4 +3,4 @@
 
 (setq inferior-lisp-program "sbcl")
 (slime-setup)
-(add-to-list 'lisp-mode-hook 'flyspell-prog-mode) ; flyspell-prog-mode isn't like other minor modesq
+(add-to-list 'lisp-mode-hook 'flyspell-prog-mode) ; flyspell-prog-mode isn't like other minor modes
