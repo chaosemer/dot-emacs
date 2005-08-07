a3a0c9fda14471ea149ec72c57f119c0d0ac09a2
jared <jared@4aac56dc-aff8-45ad-a6cf-605ace86fc27>
2005-08-07 20:48:52 +0000

    Adding ediff-binary-files3.
    git-svn-id: file:///home/jared/Desktop/vc/svn-dot-emacs/trunk@27 4aac56dc-aff8-45ad-a6cf-605ace86fc27

--- diff.el
+++ diff.el
@@ -16,7 +16,10 @@
 (defun ediff-binary-files (file1 file2)
   (interactive (list (read-file-name "File A to compare: ")
                      (read-file-name "File B to compare: ")))
-  
-  (let ((buffer1 (hexl-find-file-noselect file1))
-        (buffer2 (hexl-find-file-noselect file2)))
-    (ediff-buffers buffer1 buffer2)))
+  (apply #'ediff-buffers (mapcar #'hexl-find-file-noselect (list file1 file2))))
+
+(defun ediff-binary-files3 (file1 file2 file3)
+  (interactive (list (read-file-name "File A to compare: ")
+                     (read-file-name "File B to compare: ")
+                     (read-file-name "File C to compare: ")))
+  (apply #'ediff-buffers3 (mapcar #'hexl-find-file-noselect (list file1 file2 file3))))
