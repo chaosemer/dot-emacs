fefa9abd8e323ec8f4207f2c21b45ee1e21c83cc
jared <jared@4aac56dc-aff8-45ad-a6cf-605ace86fc27>
2005-09-27 00:42:27 +0000

    Syncing with Emacs 21.2.
    git-svn-id: file:///home/jared/Desktop/vc/svn-dot-emacs/trunk@33 4aac56dc-aff8-45ad-a6cf-605ace86fc27

--- c.el
+++ c.el
@@ -3,7 +3,11 @@
   hide-ifdef-mode
   (c-set-offset 'case-label '+)
   (setf comment-start "/* "
-        comment-end " */"))
+        comment-end " */"
+        (local-key-binding (kbd "C-c M-<right>")) 'c-forward-conditional
+        (local-key-binding (kbd "C-c M-<left>")) 'c-backward-conditional
+        (local-key-binding (kbd "C-c M-<up>")) 'c-up-conditional-with-else
+        (local-key-binding (kbd "C-c M-<down>")) 'c-down-conditional))
 
 (setf (default-value 'c-recognize-knr-p) nil
       (default-value 'c-recognize-paren-inits) t
