8e2e73620de49052418e4f95b6edea6c5e5f53e3
jared <jared@4aac56dc-aff8-45ad-a6cf-605ace86fc27>
2005-11-18 19:46:48 +0000

    Repository : :pserver:tails:/var/lib/cvs Module     : dot-emacs Working dir: ~/.emacs.d/
    In directory .:
                  Modified                00.required-libraries.el
                  Modified                custom.el
                  Modified                elisp.el
                  Modified                emacs.el
                  Modified                xml.el
    
    --------------------- Empty ---------------------
    -- last cmd: cvs -f update -d -P --
    
    
    git-svn-id: file:///home/jared/Desktop/vc/svn-dot-emacs/trunk@38 4aac56dc-aff8-45ad-a6cf-605ace86fc27

--- elisp.el
+++ elisp.el
@@ -3,9 +3,34 @@
 (hook-minor-mode emacs-lisp-mode-hook
   (setf (local-key-binding (kbd "M-.")) 'find-thing))
 
+(defun read-symbol-name (prompt &optional default-value)
+  "Read a symbol name, defaulting to DEFAULT-VALUE"
+  (let ((read (completing-read prompt obarray)))
+    (if (zerop (length read))
+        default-value
+      read)))
+
+(defun* find-symbol (string &optional (obarray obarray))
+  "Locate a symbol whose name is STRING in OBARRAY.
+
+If a symbol named STRING is found in package, th symbol found is
+returned as the first value; the second value is t.  Othrewise
+returns `(values nil nil)'"
+  (do-symbols (symbol obarray)
+    (when (string= string (symbol-name symbol))
+      (return-from find-symbol (values symbol t))))
+  (values nil nil))
+
 (defun find-thing (symbol)
   "Read and return an interned symbol, defaulting to the one near point."
-  (interactive (list (symbol-at-point)))
+  (interactive (let ((default (symbol-at-point)))
+                 (list (if default
+                           (read-symbol-name (format "Find symbol (default %s): " (symbol-name default)) (symbol-name default))
+                         (read-symbol-name "Find symbol: ")))))
+
+  (when (stringp symbol)
+    (multiple-value-bind (interned accesable?) (find-symbol symbol)
+      (setf symbol (and accesable? interned))))
 
   (find-function-do-it symbol
                        (cond ((facep symbol) 'defface)
