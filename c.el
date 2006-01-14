0bf6e9789fce9676487029cffb274f40b29fc964
jared <jared@4aac56dc-aff8-45ad-a6cf-605ace86fc27>
2006-01-14 17:46:27 +0000

    Resolving conflict.
    git-svn-id: file:///home/jared/Desktop/vc/svn-dot-emacs/trunk@44 4aac56dc-aff8-45ad-a6cf-605ace86fc27

--- c.el
+++ c.el
@@ -2,7 +2,9 @@
 (hook-minor-mode c-mode-common-hook
   hide-ifdef-mode
   (c-set-offset 'case-label '+)
+  (c-set-offset 'innamespace 0)
   (c-set-offset 'arglist-intro '++)
+  (c-set-offset 'member-init-intro '++)
   (setf (local-key-binding (kbd "C-c M-<right>")) 'c-forward-conditional
         (local-key-binding (kbd "C-c M-<left>")) 'c-backward-conditional
         (local-key-binding (kbd "C-c M-<up>")) 'c-up-conditional-with-else
