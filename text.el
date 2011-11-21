9ab72c49736812913e2c839ca65e1c369d560c15
Jared Finder <jared@hpalace.com>
2011-11-20 22:52:34 -0800

    Force-loading longlines library, prevents error when first entering a text mode.

--- text.el
+++ text.el
@@ -1,6 +1,8 @@
 ;;;; Text mode customizations.
 ;;;;
 ;;;; Why not use Emacs for editting plain old text?
+(require 'longlines)
+
 (hook-mode text-mode-hook
   (unless (member major-mode '(mail-mode org-mode))
 	longlines-mode))
