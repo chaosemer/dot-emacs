94e493889b83cb35f6c6e0688dbd7c6f262e6a0a
Jared Finder <jared@finder.org>
2020-11-02 23:39:34 -0800

    Making forward and backward char work well with nybble insertions.

--- hexl.el
+++ hexl.el
@@ -16,8 +16,7 @@
     (error "Invalid nybble %c" last-command-event))
 
   (dotimes (_ arg)
-    (let* ((address (hexl-current-address t))
-           (hex-position (hexl-address-to-marker address))
+    (let* ((hex-position (hexl-address-to-marker (hexl-current-address)))
            (byte-str (buffer-substring hex-position (+ hex-position 2)))
 
            ;; nybble index
@@ -35,6 +34,28 @@
       (hexl-insert-nybble last-command-event arg)
     (hexl-insert-multibyte-char last-command-event arg)))
 
+(defun hexl-my-backward-char (arg)
+  "Move to left ARG bytes (right if ARG negative) in Hexl mode."
+  (interactive "p")
+  (hexl-my-forward-char (- arg)))
+
+
+(defun hexl-my-forward-char (arg)
+  "Move to right ARG bytes (left if ARG negative) in Hexl mode."
+  (interactive "p")
+  (let* ((hex-position (hexl-address-to-marker (hexl-current-address)))
+         (index (if (= (point) (1+ hex-position))
+                    1 0)))
+    (hexl-goto-address (+ (hexl-current-address)
+                          arg
+                          (if (and (< arg 0) (/= index 0))
+                              1 0)))))
+
+
 (define-key hexl-mode-map (kbd "<remap> <self-insert-command>")
   'hexl-my-self-insert-command)
 (define-key hexl-mode-map (kbd "\C-m") 'hexl-my-self-insert-command)
+(define-key hexl-mode-map (kbd "<left>") 'hexl-my-backward-char)
+(define-key hexl-mode-map (kbd "<right>") 'hexl-my-forward-char)
+(define-key hexl-mode-map (kbd "\C-f") 'hexl-my-backward-char)
+(define-key hexl-mode-map (kbd "\C-b") 'hexl-my-forward-char)
