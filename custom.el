ec5b6c441a5859ee6f8f924442a65c6d5ef8b18c
Jared Finder <jared@finder.org>
2021-02-16 12:20:31 -0800

    Gotta autorevert fast!

--- custom.el
+++ custom.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-interval 0.02)
  '(c-default-style "stroustrup")
  '(calculator-electric-mode t)
  '(calculator-number-digits 10)
