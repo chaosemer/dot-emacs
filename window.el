c41137e026e8745dcc3c1d66313fcd9d96079a0e
Jared Finder <jared@finder.org>
2023-07-14 23:44:46 -0700

    Initial customization using display-buffer-alist.
    * Make transient windows pop up in a dedicated side window.
    * Make Version Control list pop up in its dedicated side window.
    * Some experiments around C-x o and side windows.
    
    I'm not sure if I'll like this setup, but it's worth trying!

---
+++ window.el
@@ -0,0 +1,29 @@
+;;;; Control the window layout. -*- lexical-binding: t; -*-
+;;;;
+;;;; Generally this customizes display-buffer-alist and related
+;;;; options.
+
+;; Make more switching commands do what I want.
+(setf switch-to-buffer-obey-display-actions t
+      switch-to-buffer-in-dedicated-window 'pop)
+
+(let ((bottom-side '(display-buffer-in-side-window
+                     (side . bottom)
+                     (window-parameters (no-other-window . t))))
+      (left-side '(display-buffer-in-side-window
+                   (side . left)
+                   (window-parameters (no-other-window . t)))))
+  (setf display-buffer-alist
+        `(;; Extremely transient windows
+          ("\\*Help\\*" ,@bottom-side)
+          ("\\*grep\\*" ,@bottom-side)
+          ("\\*Occur\\*" ,@bottom-side)
+          ("\\*Warnings\\*" ,@bottom-side)
+          ("\\*Completions\\*" ,@bottom-side)
+          ("\\*Buffer List\\*" ,@bottom-side)
+          ("\\*compilation\\*" ,@bottom-side)
+          ("\\*Apropos\\*" ,@bottom-side)
+          ("\\*Messages\\*" ,@bottom-side)
+
+          ;; VC prompts
+          ("\\*vc-dir\\*" ,@left-side))))
