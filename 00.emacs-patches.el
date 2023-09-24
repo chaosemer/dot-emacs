362abe4c38ae717dd22a2d2b324ee695547eafef
Jared Finder <jared@finder.org>
2023-09-24 13:08:51 -0700

    Patching `pixel-scroll-precision' to work on macOS.

--- 00.emacs-patches.el
+++ 00.emacs-patches.el
@@ -44,3 +44,46 @@ Image types are symbols like `xbm' or `jpeg'."
     (setf (lookup-key log-edit-mode-map (kbd "<remap> <beginning-of-line>"))
 	  (lookup-key log-edit-mode-map (kbd "C-a")))
     (setf (lookup-key log-edit-mode-map (kbd "C-a")) nil)))
+
+;; TODO(upstream)
+;;
+;; The proper upstream is actually in `pixel-scroll-precision' to
+;; collapse other cases to mouse. Solve via advice.
+(defvar device-class--should-collapse-to-mouse nil
+  "Internal variable to track if collapsing should happen")
+(defvar device-class--mouse-wheel-events
+  (list mouse-wheel-up-event
+        mouse-wheel-up-alternate-event
+        mouse-wheel-down-event
+        mouse-wheel-down-alternate-event))
+(defvar device-class--prev-event-timestamp 0
+  "Interval variable of the previous event's timestamp.")
+(defvar device-class--prev-event-seems-like-mouse-event nil)
+
+(defun device-class--collapse-to-mouse (r)
+  "Patch for `pixel-scroll-precision' to return mouse for pointers."
+  (if (and device-class--should-collapse-to-mouse
+           (eq r 'core-pointer))
+      'mouse
+    r))
+(defun pixel-scroll-precision--patched (event)
+  (interactive "e")
+  (let* ((wheel-event (member (event-basic-type event) device-class--mouse-wheel-events))
+         (line-count (event-line-count event))
+         (seems-like-mouse-event (and wheel-event (/= line-count 0)))
+         (timestamp (posn-timestamp (event-start event)))
+         ;; collapse to mouse when the event seems like a mouse event
+         (device-class--should-collapse-to-mouse
+          (if (< (- timestamp device-class--prev-event-timestamp)
+                 100)
+              device-class--prev-event-seems-like-mouse-event
+            seems-like-mouse-event)))
+    (pixel-scroll-precision event)
+    (setf device-class--prev-event-timestamp timestamp
+          device-class--prev-event-seems-like-mouse-event device-class--should-collapse-to-mouse)))
+
+(when (and (featurep 'ns-win)
+           (version<= "29.1" emacs-version))
+  (display-warning 'emacs "Fixing buggy pixel-scroll-precesion")
+  (define-key global-map (kbd "<remap> <pixel-scroll-precision>") 'pixel-scroll-precision--patched)
+  (advice-add 'device-class :filter-return 'device-class--collapse-to-mouse))
