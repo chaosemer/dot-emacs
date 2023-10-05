a41f50b5a4ccecc1a3856ccfaa3ceaa3e68fc224
Jared Finder <jared@finder.org>
2023-10-05 13:42:45 -0700

    Make markdown-mode work more smoothly for my workflow.

--- text.el
+++ text.el
@@ -4,3 +4,13 @@
 (hook-mode text-mode-hook
   (unless (member major-mode '(mail-mode org-mode))
 	visual-line-mode))
+
+(hook-mode markdown-mode-hook
+  ;; Normally, electric pair mode is on and helpful, but in Markdown
+  ;; mode it makes URLs be automatically hidden before they're typed
+  ;; in.
+  (electric-pair-local-mode -1)
+
+  (setf markdown-hide-markup t
+        ;; So URLs hide automatically as they are typed.
+        markdown-hide-urls t))
