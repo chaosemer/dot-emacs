3c8e52cb546c6d96bed5c5f55293c7418011d1ec
Jared Finder <jared@finder.org>
2023-11-14 10:10:36 -0800

    Removing customizations no longer needed in Emacs 29.

--- menu-bar.el
+++ menu-bar.el
@@ -1,22 +1,4 @@
 ;;;; Menu bar customizations  -*- lexical-binding: t; -*-
 ;;;;
-;;;; Customizing the Emacs menu-bar. Mostly for TTYs (that's what I
-;;;; used on Windows), but there's some GUI customizations here too.
-;;;;
-;;;; This probably should just get upstreamed.
-;;;;
-;;;; TODO: Make x-popup-menu properly handle mouse input (it treats a
-;;;; mouse-click as "select current entry")
-
-;; Clean up the file menu
-(keymap-set menu-bar-file-menu "<new-file>"
-  '(menu-item "New File"
-              (lambda ()
-                (interactive)
-                (switch-to-buffer (generate-new-buffer "untitled")))
-              :enable (menu-bar-non-minibuffer-window-p)
-              :help "Create a new buffer"))
-(keymap-set menu-bar-file-menu "<open-file>"
-  '(menu-item "Open File..." find-file
-              :enable (menu-bar-non-minibuffer-window-p)
-              :help "Open an existing file"))
+;;;; TODO: Make x-popup-menu on TTYs properly handle mouse input (it
+;;;; treats a mouse-click as "select current entry")
