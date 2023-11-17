62d06b34e60f876fd7896bf05c64c6f8e525c2a6
Jared Finder <jared@finder.org>
2023-11-16 22:32:34 -0800

    Explicitly not making early-init only changes during (regular) init.

--- early-init.el
+++ early-init.el
@@ -1,15 +1,18 @@
-;;;; Visual changes that affect the initial frame.
-;;;;
-;;;; By using early-init.el, we can avoid flickering.
+;;;; Visual changes that affect the initial frame.  -*- lexical-binding: t; -*-
 
-;; Dark mode
-(setf modus-themes-headings
-      '((1 . (variable-pitch (height 1.5)))
-        (2 . (variable-pitch (height 1.3)))
-        (3 . (variable-pitch (height 1.1)))
-        (t . (t))))
-(load-theme 'modus-vivendi t)
+;; This file should only be loaded during the early init and not
+;; during regular init. `early-init-file' is set after the early init
+;; process, which serves as a clear flag.
+(unless early-init-file
+  (setf modus-themes-headings
+        '((1 . (variable-pitch (height 1.5)))
+          (2 . (variable-pitch (height 1.3)))
+          (3 . (variable-pitch (height 1.1)))
+          (t . (t))))
 
-;; Maximized (but not its own desktop on Mac)
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(tool-bar-mode -1)
+  ;; Dark mode
+  (load-theme 'modus-vivendi t)
+
+  ;; Maximized (but not its own desktop on Mac)
+  (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+  (tool-bar-mode -1))
