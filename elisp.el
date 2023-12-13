49255b5f5a8e099b033da750aa5432e149900150
Jared Finder <jared@finder.org>
2023-12-12 18:44:53 -0800

    emacs-lisp-mode-map no longer has a binding for TAB.

--- elisp.el
+++ elisp.el
@@ -10,8 +10,3 @@
 (font-lock-add-keywords 'emacs-lisp-mode
                         '(("^\\s *;;;.*\n?" (0 'section-comment-face t))
                           ("^;;;;.*\n?" (0 'file-comment-face t))))
-
-;;; Keymaps:
-
-;; emacs-lisp-mode defines the tab key in its map.  It shouldn't TODO(upstream)
-(keymap-unset emacs-lisp-mode-map "TAB")
