5f6e94c4866b011d2923fef4a199e34e4a897eb5
Jared Finder <jared@finder.org>
2025-06-11 11:52:58 -0700

    Accept ANSI color escape sequences since they show up in a handful of tools

--- compilation.el
+++ compilation.el
@@ -6,3 +6,7 @@
   (dotimes (_ 2)
     (beep) (sleep-for 0.2)))
 (add-hook 'compilation-finish-functions 'road-runner)
+
+;; Accept ANSI color escape sequences since they show up in a handful
+;; of tools
+(add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
