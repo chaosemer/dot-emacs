27f81358fff787a95f8d21fa7a0eb3ebee669ff0
Jared Finder <jared@finder.org>
2026-08-26 11:13:58 -0700

    Take advantage of new Emacs 31 in elisp.el

--- elisp.el
+++ elisp.el
@@ -1,6 +1,8 @@
 ;;; init/elisp.el --- Emacs Lisp customizations  -*- lexical-binding: t; -*-
 
 ;;; Declarations:
+(defvar elisp-fontify-semantically)
+(defvar elisp-add-help-echo)
 (defvar ielm-dynamic-return)
 (defvar ielm-mode-hook)
 
@@ -11,7 +13,12 @@
 (add-hook 'emacs-lisp-mode-hook #'my-emacs-lisp-mode-hook)
 (add-hook 'ielm-mode-hook #'my-emacs-lisp-mode-hook)
 
-(setf ielm-dynamic-return 'point)
+(setf ielm-dynamic-return 'point
+      elisp-fontify-semantically t
+      ;; Adding help echo spams way too much data in the echo area.
+      ;; For now, just silence it, maybe a future Emacs version makes
+      ;; this usable.
+      elisp-add-help-echo nil)
 
 ;;; Faces:
 (font-lock-add-keywords 'emacs-lisp-mode
