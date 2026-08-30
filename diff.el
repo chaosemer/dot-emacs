6a9534912cbd015ee5279b85ddae175a665eee5a
Jared Finder <jared@finder.org>
2026-08-30 16:00:39 -0700

    Also restore window config after ediff dirs

--- diff.el
+++ diff.el
@@ -16,16 +16,39 @@
 (defvar my-ediff-prev-window-configuration nil
   "The window configuration before entering ediff.")
 
-(add-hook 'ediff-before-setup-hook
-          (defun my-ediff-save-window-configuration ()
-            (setf my-ediff-prev-window-configuration
-                  (current-window-configuration))))
+(defun my-ediff-save-window-configuration ()
+  (let ((window-config (current-window-configuration)))
+    ;; Deleting other windows might error, so do it before modifying
+    ;; the saved configuration.
+    (delete-other-windows)
+    (setf my-ediff-prev-window-configuration window-config)))
+(defun my-ediff-restore-window-configuration ()
+  (set-window-configuration my-ediff-prev-window-configuration))
 
-(add-hook 'ediff-quit-hook
-          (defun my-ediff-restore-window-configuration ()
-            (set-window-configuration my-ediff-prev-window-configuration)))
-(add-hook 'ediff-suspend-hook 'my-ediff-restore-window-configuration)
-;; no special code
+(add-hook 'ediff-before-directory-setup-hooks
+          #'my-ediff-save-window-configuration)
+(add-hook 'ediff-before-setup-hook #'my-ediff-save-window-configuration)
+(add-hook 'ediff-quit-hook #'my-ediff-restore-window-configuration)
+(add-hook 'ediff-quit-session-group-hook
+          #'my-ediff-restore-window-configuration)
+(add-hook 'ediff-suspend-hook #'my-ediff-restore-window-configuration)
+
+;; Don't show the registry when quitting directories
+(defvar in-ediff-quit-meta-buffer nil)
+(defun dont-show-registry-buffer-when-quitting-directories (old-fn)
+ "Advice function for `ediff-quit-meta-buffer'.
+
+OLD-FN: Old function, provided by advice system."
+ (let ((in-ediff-quit-meta-buffer t))
+   (funcall old-fn)))
+(defun dont-show-registry (old-fn &rest _args)
+  "Advice function for `ediff-show-registry'."
+  (unless in-ediff-quit-meta-buffer
+    (funcall old-fn)))
+
+(advice-add 'ediff-quit-meta-buffer :around
+            'dont-show-registry-buffer-when-quitting-directories)
+(advice-add 'ediff-show-registry :around 'dont-show-registry)
 
 ;;; Kemaps:
 ;; I prefer customized interfaces for viewing diffs.
